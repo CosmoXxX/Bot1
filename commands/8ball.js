8ee97cc0081c1e7e048a6142107bc494f0682ebe
CosmoXxX <50768054+CosmoXxX@users.noreply.github.com>
2019-09-25 18:44:06 +0400

    Create 8ball.js

---
+++ commands/8ball.js
@@ -0,0 +1,12 @@
+module.exports.run = (client, message, args) => {
+    if (!args[1]) return message.reply("S'il te plaît, pose-moi une question !"); 
+    let replies = ["Oui", "Non", "Peut-être", "Probablement que oui", "Probablement que non","Surement","Je pense que oui","Bien sûr!","Mes sources disent non.","Je sais pas"];
+    let res = Math.floor(Math.random() * replies.length);  
+    message.channel.send(`:8ball: | ${replies[res]}`)
+};
+
+module.exports.config = {
+    name: '8ball',
+aliases: []
+};
+ 
