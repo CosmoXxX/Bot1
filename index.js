7f9aa12ea7be81b8864a5dc1d9704ec4f08f6483
CosmoXxX <50768054+CosmoXxX@users.noreply.github.com>
2019-09-27 18:27:01 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,37 +1,54 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const { Client, Collection } = require("discord.js");
+const { config } = require("dotenv");
+const fs = require("fs");
 
-client.on("ready", async () => {
-    console.log(`${client.user.username} is now Active!`);
-    client.user.setActivity('*help');
-})
+const client = new Client({
+    disableEveryone: true
+});
 
-const fs = require('fs');
-client.commands = new Discord.Collection();
+client.commands = new Collection();
+client.aliases = new Collection();
 
-fs.readdir("./commands/", (err, files) => {
-    if(err) console.log(err);
+client.categories = fs.readdirSync("./commands/");
 
-    const jsfile = files.filter(f => f.endsWith('.js'));
-    if (jsfile.length <= 0) return console.log("[FS] Couldn't Find Commands!");
+config({
+    path: __dirname + "/.env"
+});
 
-    jsfile.forEach((f, i) => {
-        let pull = require(`./commands/${f}`);
-        client.commands.set(pull.config.name, Object.assign(pull, { 
-            triggers: [ pull.config.name, ...(pull.config.aliases || []) ]
-        }));
-    })
+["command"].forEach(handler => {
+    require(`./handlers/${handler}`)(client);
+});
+
+client.on("ready", () => {
+    console.log(`Hi, ${client.user.username} is now online!`);
+
+    client.user.setPresence({
+        status: "online",
+        game: {
+            name: "*help | !CR3W",
+            type: "STREAMING"
+        }
+    }); 
 });
 
 client.on("message", async message => {
-    const prefix = "*";
-    if(message.author.bot || message.channel.type === 'dm') return;
-    if(!message.content.startsWith(prefix)) return;
+    const prefix = "_";
 
-    const [ cmd, ...args ] = message.content.slice(prefix.length).split(/ +/g) 
+    if (message.author.bot) return;
+    if (!message.guild) return;
+    if (!message.content.startsWith(prefix)) return;
+    if (!message.member) message.member = await message.guild.fetchMember(message);
 
-    let commandFile = client.commands.find(c => c.triggers.includes(cmd.toLowerCase()));
-    if(commandFile) commandFile.run(client, message, args)
-})
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const cmd = args.shift().toLowerCase();
+    
+    if (cmd.length === 0) return;
+    
+    let command = client.commands.get(cmd);
+    if (!command) command = client.commands.get(client.aliases.get(cmd));
 
-client.login("NjIwMTM3NDA0Nzk5NjQ3NzYz.XYs0xA.KniveUAqgAnhhorwcyvayT7vSo0")
+    if (command) 
+        command.run(client, message, args);
+});
+
+client.login(process.env.TOKEN);
